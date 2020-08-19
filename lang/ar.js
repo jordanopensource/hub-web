d7b950c3587b732049858d97a1965cdabfc7c6d2
wissam <wissam_ataleh@outlook.com>
2020-08-19 17:08:26 +0300

    Add arabic and english translation files

---
+++ lang/ar.js
@@ -0,0 +1,50 @@
+export default {
+  // Contact Us
+  contact: {
+    address: 'العنوان',
+    workingHours: 'ساعات العمل',
+    voice: 'هاتف',
+    email: 'ايميل',
+    connect: 'تواصل معنا'
+  },
+  feedback: {
+    title: 'Feedback and Inquiries',
+    description: 'Whoever you are, whether you are happy or unhappy about any aspect of our activity or work, we value you feedback.',
+    fillForm: 'If you have a comment or complaint, please fill in our online feedback form below.'
+  },
+  // Auth
+  auth: {
+    login: "تسجيل الدخول",
+    logout: "تسجيل الخروج",
+    email: 'الايميل او اسم المستخدم',
+    password: 'كلمة المرور'
+  },
+  // Meta
+  meta: {
+    writtenBy: 'كتبه',
+    readTheReport: 'إقرأ التقرير',
+    knowMore: 'اعرف أكثر',
+    by: 'من',
+    download: 'تنزيل',
+    closed: 'مغلق',
+    visitWebsite: 'زيارة الموقع',
+    email: 'الايميل'
+  },
+  // Members
+  members: {
+    members: 'عضو'
+  },
+  // Sort
+  sort: {
+    sortBy: 'الترتيب حسب',
+    memberSinceACS: 'تاريخ التسجيل: تصاعدي',
+    memberSinceDESC: 'تاريخ التسجيل: تنازلي',
+    fullNameASC: 'الأسم: تصاعدي',
+    fullNameDESC: 'الأسم: تنازلي'
+  },
+  // Pagination
+  pagination: {
+    next: 'التالي',
+    prev: 'السابق'
+  }
+}
