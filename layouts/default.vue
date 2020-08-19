b0feefd38cd89bb52d0bd2cb6689999cab42e3ad
wissam <wissam_ataleh@outlook.com>
2020-08-19 17:18:27 +0300

    Add global components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,55 +1,36 @@
 <template>
-  <div>
-    <nuxt />
+  <div :dir="$dir()">
+    <mainHeader />
+    <nuxt class="bg-josa-warm-grey-light pb-8" />
+    <mainFooter />
   </div>
 </template>
 
+<script>
+  import mainHeader from '~/components/UI/Header';
+  import mainFooter from '~/components/UI/Footer';
+
+  export default {
+    head() {
+      const i18nSeo = this.$nuxtI18nSeo()
+      return {
+        title: 'Jordan Open Source Association',
+        meta: [{
+            hid: 'home-page',
+            name: 'Home Page',
+            content: 'JOSA Portal Web App'
+          },
+          ...i18nSeo.meta
+        ]
+      }
+    },
+    components: {
+      mainHeader,
+      mainFooter
+    },
+  }
+
+</script>
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
 
-*,
-*:before,
-*:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
 </style>
