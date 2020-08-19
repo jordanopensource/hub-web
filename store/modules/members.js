0f291896e6b30048f0f61bd3b4dfc343f958bc86
wissam <wissam_ataleh@outlook.com>
2020-08-19 17:08:04 +0300

    Add members, menus and auth store files

---
+++ store/modules/members.js
@@ -0,0 +1,64 @@
+// Members Store
+import axios from 'axios';
+
+const state = {
+  loadedMembers: [],
+  membersCount: 1
+};
+
+const getters = {
+  loadedMembers(state) {
+    return (state.loadedMembers)
+  },
+  membersCount(state) {
+    return (state.membersCount)
+  }
+};
+
+const mutations = {
+  setMembers(state, members) {
+    state.loadedMembers = members
+  },
+  setMembersCount(state, count) {
+    state.membersCount = count
+  },
+  addMember(state, user) {
+    state.loadedMembers.push(user)
+  }
+};
+
+const actions = {
+  async fetchMembers(vuexContext, context) {
+    await axios
+      .get(process.env.baseUrl + "/users?_sort=memberSince:DESC")
+      .then(res => {
+        const membersArray = []
+        for (const key in res.data) {
+          membersArray.push({
+            ...res.data[key]
+          })
+        }
+        vuexContext.commit("setMembers", membersArray);
+      })
+      .catch(e => context.error(e));
+  },
+  async countMembers(vuexContext, context) {
+    await axios
+      .get(process.env.baseUrl + "/users/count")
+      .then(res => {
+        const count = res.data
+        vuexContext.commit("setMembersCount", count);
+      })
+      .catch(e => context.error(e));
+  },
+  newUser(vuexContext, user) {
+    return axios.post(process.env.baseUrl + "/auth/local/register", user)
+  },
+};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
