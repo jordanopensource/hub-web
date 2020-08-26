21c74f09c31f63ccd9c70c0d2f362f017f63c3b8
wissam <wissam_ataleh@outlook.com>
2020-08-27 01:40:09 +0300

    Add fontawesome solid icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,6 +71,10 @@ export default {
   fontawesome: {
     imports: [
       {
+        set: '@fortawesome/free-solid-svg-icons',
+        icons: ['fas']
+      },
+      {
         set:'@fortawesome/free-brands-svg-icons',
         icons: ['fab']
       }
