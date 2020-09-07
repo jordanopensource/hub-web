bcdf8c777053e7e6036b520e06a0879293afc404
wissam <wissam_ataleh@outlook.com>
2020-09-07 10:27:59 +0300

    Add title class to heading

--- pages/post-registration.vue
+++ pages/post-registration.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="px-24 pt-24 pb-40 my-12 bg-white">
-      <h2>{{ $t('regForm.welcome') }}</h2>
+      <h2 class="title">{{ $t('regForm.welcome') }}</h2>
       <p>{{ $t('regForm.thanks') }}</p>
     </div>
   </div>
