3d8776126d9d8e61acf06e3a669a395880513642
wissam <wissam_ataleh@outlook.com>
2020-09-08 12:40:16 +0300

    Add my profile menu item

--- components/Auth/LogInOutMenu.vue
+++ components/Auth/LogInOutMenu.vue
@@ -1,8 +1,11 @@
 <!-- Login/Logout Menu Item -->
 <template>
-  <span class="nav-link">
-    <button v-if="$store.getters.auth" @click="logout">{{ $t('auth.logout') }}</button>
-    <NuxtLink v-else to="/login">{{ $t('auth.login') }}</NuxtLink>
+  <span>
+    <span v-if="$store.getters.auth">
+      <NuxtLink class="nav-link" to="/me">My Profile</NuxtLink>
+      <button class="nav-link" @click="logout">{{ $t('auth.logout') }}</button>
+    </span>
+    <NuxtLink class="nav-link" v-else to="/login">{{ $t('auth.login') }}</NuxtLink>
   </span>
 </template>
 
