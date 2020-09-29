aed47be5331a1c1561b2ca28817cd005aafda698
wissam <wissam_ataleh@outlook.com>
2020-09-29 11:53:15 +0300

    Delete homeRedirect middleware

--- middleware/homeRedirect.js
+++
@@ -1,8 +0,0 @@
-export default function ({ store, redirect }) {
-    // Define homepage if user is authenticated or not
-    if (store.getters.auth) {
-      return redirect('/me')
-    } else {
-      return redirect('/join')
-    }
-  }
