e8ca7a411d51dd9e0076afe495e7aa7592f00cf9
wissam <wissam_ataleh@outlook.com>
2020-09-29 11:43:30 +0300

    Add get best section

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div id="homepage">
     <banner :title="meta['title_' + $i18n.locale]" :description="meta['description_' + $i18n.locale]" />
     <stepsList :steps="steps" class="mt-12" />
+    <getBest :data="hubBest" />
   </div>
 </template>
 <script>
   import axios from 'axios';
   import banner from '~/components/Home/Banner';
   import stepsList from '~/components/Steps/StepsList';
+  import getBest from '~/components/Home/GetBest';
   export default {
     components: {
       banner,
-      stepsList
+      stepsList,
+      getBest
     },
     head() {
       const i18nSeo = this.$nuxtI18nSeo()
@@ -29,11 +32,19 @@
     async asyncData(context) {
       const pageMeta = await axios.get(process.env.baseUrl + '/page-metas?pageId=hubHome');
       const steps = await axios.get(process.env.baseUrl + '/become-member');
+      const hubBest = await axios.get(process.env.baseUrl + '/get-best-hub');
       return {
         meta: pageMeta.data[0],
-        steps: steps.data
+        steps: steps.data,
+        hubBest: hubBest.data
       }
     },
   }
 
 </script>
+
+<style scoped>
+  #homepage {
+    padding-bottom: 0;
+  }
+</style>
