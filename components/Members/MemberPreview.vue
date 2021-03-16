a2db5380b840046c68069abb8c8b9243a8704637
Ashaqra <abdallashaqra2000@gmail.com>
2021-03-16 09:42:06 +0200

    Update member info style. Make badges smaller.

--- components/Members/MemberPreview.vue
+++ components/Members/MemberPreview.vue
@@ -5,55 +5,55 @@
       <appImage v-if="member.profilePicture" :image="member.profilePicture" size="small" class="profilePicture ltr:mr-4 rtl:ml-4 mb-2" />
       <img v-else class="profilePicture ltr:mr-4 rtl:ml-4 mb-2" src="/images/bots/superbot.svg" />
     </nuxt-link>
-    <!-- Full Name -->
-    
+   
+    <!-- The Left Block -->
       <div class='mx-4 relative flex-grow'>
+         <!-- Full Name -->
         <nuxt-link :to="memberLink">
           <h2 class="block">{{ member['fullName_' + $i18n.locale] | capitalize }}</h2>
         </nuxt-link>
-      <!-- Membership Type -->
-      <!-- <p class="my-2">{{ member.membershipType | capitalize }}</p> -->
-      <!-- Badges -->
-      <!-- <badges v-if="member.badges" :badges="member.badges" /> -->
+        <!-- Title -->      
         <div v-if="titles">
             <p v-for="(title,index) in titles" :key="index" class="text-sm block">{{ title }}</p>
         </div>
-      <div >
-        <section class="md:absolute bottom-0 left-0" v-if="interests.length">
-          
-            <div class="-mx-3">
-              <span v-for="(interest,index) in interests.slice(0,3)" :key="index" class="interest inline-block">
-                {{ interest }}
-              </span>
-            </div>
+      <!-- Intersets Section -->
+        <div >
+          <section class="md:absolute bottom-0 left-0" v-if="interests.length">
+              <div class="-mx-3">
+                <span v-for="(interest,index) in interests.slice(0,3)" :key="index" class="interest inline-block">
+                  {{ interest }}
+                </span>
+              </div>
           </section>
         </div>
       </div>
-       
+       <!-- The Right Block -->
       <div class="relative space-y-4">
-        <div class="flex md:justify-end">
-          <span :class="[member.membershipType == 'active' ? 'text-josa-blue-dark':'text-josa-blue']"><font-awesome-icon  icon="circle" size="xs" class="m-1"/></span>
-          <p class="member-id mr-2 font-bold text-sm">{{ member.membershipId }}</p>
-          <p class="text-sm text-gray-600">Member for {{ member.memberSince ? dateTrans(member.memberSince, "YYYYMMDD"): dateTrans(member.created_at, "YYYYMMDD") | dateTrans(member.created_at, "YYYYMMDD")}}
-          </p>
-        </div>
-       <div v-show="member.solvedTasks.length!=0 && auth!=null" class="flex md:justify-end"> 
-          <font-awesome-icon  icon="star" size="xs" class="m-1"/>
-          <p class="text-sm ml-1">Score:</p>
-          <p v-for="task in member.solvedTasks" :key="task.id" class="text-sm"> {{ task.points }} </p>
-       </div>
-        <div v-show="auth!=null" class="flex md:justify-end">
-            <font-awesome-icon  icon="clock" size="xs" class="m-1"/>
-              
-            <p class="text-sm ml-1">Last seen online:</p>
-        </div>
-        <div class="flex md:absolute bottom-0 right-0 md:justify-end">
-            <badges  v-if="member.badges" :badges="member.badges" />
-        </div>
+        <!-- Membership Type Icon -->
+          <div class="flex md:justify-end">
+            <span :class="[member.membershipType == 'active' ? 'text-josa-blue-dark':'text-josa-blue']"><font-awesome-icon  icon="circle" size="xs" class="m-1"/></span>
+            <p class="member-id mr-2 font-bold text-sm">{{ member.membershipId }}</p>
+            <p class="text-sm text-gray-600">Member for {{ member.memberSince ? dateTrans(member.memberSince, "YYYYMMDD"): dateTrans(member.created_at, "YYYYMMDD") | dateTrans(member.created_at, "YYYYMMDD")}}
+            </p>
+          </div>
+        <!-- Member Points ( Show If logged in )-->
+         <div v-show="member.solvedTasks.length!=0 && auth!=null" class="flex md:justify-end"> 
+             <font-awesome-icon  icon="star" size="xs" class="m-1"/>
+             <p class="text-sm ml-1">Score:</p>
+             <p v-for="task in member.solvedTasks" :key="task.id" class="text-sm"> {{ task.points }} </p>
+         </div>
+       <!-- Last Seen Online ( Show If logged in ) -->
+          <div v-show="auth!=null" class="flex md:justify-end">
+              <font-awesome-icon  icon="clock" size="xs" class="m-1"/>
+              <p class="text-sm ml-1">Last seen online:</p>
+          </div>
+        <!-- Badges -->
+          <div class="flex md:absolute bottom-0 right-0 md:justify-end">
+              <badges  v-if="member.badges" :badges="member.badges" />
+          </div>
       </div>
     </div>
-   
-   
+
 </template>
 
 <script>
@@ -69,17 +69,15 @@
       appImage,
       badges,
       solvedTask,
-      
     },
+
     props: {
       member: {
         type: Object,
         required: true
       },
-      
-      
-      
     },
+
     data:{
         task:Object,
     },
@@ -97,32 +95,24 @@
         }
       },
       titles() {
-        
         try {
           const titles = this.$options.filters.stringToArray(this.member.titles)
          
           return titles
         } catch {
+
           return null
         }
       },
       ...mapGetters(['auth']),
-      
-    },
+     },
+
     methods:{
       dateTrans(from,now){
-        
         return moment(from,now).fromNow().slice(0,-4);
-        
-        
-
       },
-      
-      
     }
-    
-  }
-
+ }
 </script>
 
 <style scoped>
@@ -137,9 +127,7 @@
   }
 
   /deep/ .badges img{
-     width: 40px;
-     height: 40px;
+     width: 36px;
+     height: 36px;
   }
-  
-
-</style>
+ </style>
