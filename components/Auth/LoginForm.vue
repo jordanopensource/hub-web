9d9afba0fda20cb54633c5a6de782e946dae0d2f
JBlade97 <mubaidien@gmail.com>
2021-03-21 15:51:19 +0200

    Fix email input type

--- components/Auth/LoginForm.vue
+++ components/Auth/LoginForm.vue
@@ -1,6 +1,6 @@
 <template>
   <form @submit.prevent="submit" class="w-full">
-    <AppControlInput v-model="username" controlType="input" :placeholder="$t('auth.email')"></AppControlInput>
+    <AppControlInput v-model="username" controlType="email" :placeholder="$t('auth.email')"></AppControlInput>
     <AppControlInput v-model="password" controlType="password" :placeholder="$t('auth.password')"></AppControlInput>
     <nuxt-link to='/forgot-password' class="italic text-sm text-josa-blue">{{ $t('auth.forgotPassword') }}</nuxt-link>
     <AppButton btn-style="button-blue-full" type="submit" class="block mt-4">{{ $t('auth.login') }}</AppButton>
